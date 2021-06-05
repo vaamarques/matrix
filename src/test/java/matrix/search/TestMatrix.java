955d33fcd62d507d0a101085be1d6713d5b1d933
Vera Marques <vera.marques@wit-software.com>
2021-06-05 15:53:06 +0100

    result of testes

--- src/test/java/matrix/search/TestMatrix.java
+++ src/test/java/matrix/search/TestMatrix.java
@@ -1,8 +1,13 @@
 package matrix.search;
 
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
 
 import matrix.search.model.Matrix2D;
+import matrix.search.model.MatrixElement;
+import matrix.search.model.Point;
+import matrix.search.model.Point3D;
 
 /**
  * 
@@ -12,7 +17,17 @@ import matrix.search.model.Matrix2D;
 public class TestMatrix {
 
 	@Test
-	public void textMatrix2D3x3() throws Exception {
+	public void testPoints() throws Exception {
+
+		Point x = new Point(1, 1);
+		Point3D x3D = new Point3D(1, 1, 1);
+		MatrixElement<Point> el = new MatrixElement<Point>(1, x);
+		MatrixElement<Point3D> el2 = new MatrixElement<Point3D>(2, x3D);
+		assertNotEquals(el, el2);
+	}
+	
+	@Test
+	public void testMatrix2D3x3() throws Exception {
 
 		System.out.println("Matrix2D 3x3 ");
 
@@ -21,14 +36,14 @@ public class TestMatrix {
 		matrix.fillMatrix();
 
 		matrix.printMatrix();
-		
+
 		matrix.printLongestSequence();
-		
+
 		System.out.println();
 	}
 
 	@Test
-	public void textMatrix2D4x4() throws Exception {
+	public void testMatrix2D4x4() throws Exception {
 
 		System.out.println("Matrix2D 4x4 ");
 
@@ -39,12 +54,12 @@ public class TestMatrix {
 		matrix.printMatrix();
 
 		matrix.printLongestSequence();
-		
+
 		System.out.println();
 	}
 
 	@Test
-	public void textMatrix2D6x6() throws Exception {
+	public void testMatrix2D6x6() throws Exception {
 
 		System.out.println("Matrix2D 6x6 ");
 
@@ -52,9 +67,9 @@ public class TestMatrix {
 
 		matrix.fillMatrix();
 		matrix.printMatrix();
-		
+
 		matrix.printLongestSequence();
-		
+
 		System.out.println();
 	}
 }
